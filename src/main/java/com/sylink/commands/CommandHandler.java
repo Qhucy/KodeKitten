b9e734b6f38854226a6e0cf8e8654cbdb267d85f
Qhucy <qhucy@protonmail.com>
2022-06-06 00:32:37 -0700

    Framework for handling commands

---
+++ src/main/java/com/sylink/commands/CommandHandler.java
@@ -0,0 +1,12 @@
+package com.sylink.commands;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+/**
+ * Class that handles slash command events and runs the respective command.
+ */
+public final class CommandHandler extends ListenerAdapter
+{
+
+
+}
