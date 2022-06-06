a0ea72730e8d5a16eb436579d8120154b71b3752
Qhucy <qhucy@protonmail.com>
2022-06-06 01:39:05 -0700

    Testing for accounts

--- src/main/java/com/sylink/commands/CommandHandler.java
+++ src/main/java/com/sylink/commands/CommandHandler.java
@@ -1,5 +1,9 @@
 package com.sylink.commands;
 
+import com.sylink.account.Account;
+import com.sylink.account.AccountManager;
+import lombok.NonNull;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 /**
@@ -8,5 +12,24 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public final class CommandHandler extends ListenerAdapter
 {
 
+    @Override
+    public void onSlashCommand(@NonNull final SlashCommandEvent event)
+    {
+        if (event.getUser().isBot())
+            return;
+
+        System.out.println(1);
+
+        Account account = AccountManager.getAccount(event.getUser().getIdLong());
+
+        if (account == null)
+        {
+            System.out.println("account is null");
+        }
+        else
+        {
+            System.out.printf("%d | %f\n", account.getDiscordId(), account.getBalance());
+        }
+    }
 
 }
