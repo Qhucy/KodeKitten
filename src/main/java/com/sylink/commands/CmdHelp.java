1cea9c7ae8ed7e024e5f8f0cf3fa154d5c377270
Qhucy <qhucy@protonmail.com>
2022-06-13 00:54:07 -0700

    Implementation of the help command

---
+++ src/main/java/com/sylink/commands/CmdHelp.java
@@ -0,0 +1,32 @@
+package com.sylink.commands;
+
+import com.sylink.account.Account;
+import lombok.NonNull;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+
+/**
+ * Help command that displays all commands.
+ */
+public final class CmdHelp
+        extends Command
+{
+
+    public CmdHelp()
+    {
+        super(CommandType.universal(), "Displays all commands", new String[] {"{label}"}, null, "help", "?");
+    }
+
+    @Override
+    public void onCommand(@NonNull final SlashCommandEvent event, @NonNull final Account account,
+                          @NonNull final String label, @NonNull final String[] args)
+    {
+        event.reply("List of commands:").queue();
+    }
+
+    @Override
+    public void onCommand(@NonNull final String label, @NonNull final String[] args)
+    {
+        System.out.println("List of commands:");
+    }
+
+}
