0f9787b2b9cae537be2e378e5c4eac547f3d723d
Qhucy <qhucy@protonmail.com>
2022-06-17 10:12:28 -0700

    Infrastructure for role command

---
+++ src/main/java/com/sylink/commands/CmdRole.java
@@ -0,0 +1,16 @@
+package com.sylink.commands;
+
+/**
+ *
+ */
+public final class CmdRole
+{
+
+    // console only command.
+    // allows to get information about all roles on the server
+    // e.g. displaying them
+    // viewing informatoin about a role from its id
+    // viewing roles of an account that they actually have and are on their account storage
+
+
+}
