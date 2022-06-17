7fd705183980e3d8b67d6678c3669a3a47d74c46
Qhucy <qhucy@protonmail.com>
2022-06-17 09:04:47 -0700

    Made the help message shared between user and console commands

--- src/main/java/com/sylink/commands/CmdHelp.java
+++ src/main/java/com/sylink/commands/CmdHelp.java
@@ -11,22 +11,29 @@ public final class CmdHelp
         extends Command
 {
 
+    private final static String helpMessage = """
+            **Available Commands:**
+            **/balance [user]**: View the balance of an account.
+            """;
+
     public CmdHelp()
     {
-        super(CommandType.universal(), "Displays all commands", new String[] {"{label}"}, null, "help", "?");
+        super(CommandType.universal(), "Displays all commands", new String[]{"{label}"}, null, "help", "?");
     }
 
     @Override
     public void onUserCommand(@NonNull final SlashCommandEvent event, @NonNull final Account account,
-                          @NonNull final String label, @NonNull final String[] args)
+                              @NonNull final String label, @NonNull final String[] args)
     {
-        event.reply("List of commands:").setEphemeral(true).queue();
+        event.reply("Check your DMs for command help!").setEphemeral(true).queue();
+
+        event.getUser().openPrivateChannel().complete().sendMessage(helpMessage).queue();
     }
 
     @Override
     public void onConsoleCommand(@NonNull final String label, @NonNull final String[] args)
     {
-        System.out.println("List of commands:");
+        System.out.println(helpMessage);
     }
 
 }
