cd5775d813f68d7574f4e937cd0346f069ac133c
Qhucy <qhucy@protonmail.com>
2022-06-18 10:24:53 -0700

    Temporarily changed functionality of the console implementation to test database saving / loading

--- src/main/java/com/sylink/commands/CmdHelp.java
+++ src/main/java/com/sylink/commands/CmdHelp.java
@@ -1,6 +1,7 @@
 package com.sylink.commands;
 
 import com.sylink.account.Account;
+import com.sylink.account.AccountManager;
 import lombok.NonNull;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 
@@ -33,7 +34,40 @@ public final class CmdHelp
     @Override
     public void onConsoleCommand(@NonNull final String label, @NonNull final String[] args)
     {
-        System.out.println(helpMessage);
+        //System.out.println(helpMessage);
+
+        final Account account = AccountManager.getInstance().getAccount(259508942420443136L);
+
+        if (account == null)
+        {
+            System.out.println("null");
+            return;
+        }
+
+        if (args.length == 0)
+        {
+            System.out.println(account.getDiscordId() + " | " + account.getBalance() + " | " + account.getLastActivityTime());
+            return;
+        }
+
+        if (args[0].equalsIgnoreCase("1"))
+        {
+            account.addBalance(1.0);
+
+            System.out.println(account.getBalance());
+        }
+        else if (args[0].equalsIgnoreCase("2"))
+        {
+            AccountManager.getInstance().flushFromMemory(account, false);
+        }
+        else if (args[0].equalsIgnoreCase("3"))
+        {
+            System.out.println(AccountManager.getInstance().existsInMemory(account.getDiscordId()));
+        }
+        else if (args[0].equalsIgnoreCase("4"))
+        {
+            System.out.println(AccountManager.getInstance().existsInDatabase(account.getDiscordId()));
+        }
     }
 
 }
