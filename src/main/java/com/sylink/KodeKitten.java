796b3be0d8e08e592755d3a3d86259d056ee641f
Qhucy <qhucy@protonmail.com>
2022-06-19 15:14:09 -0700

    Removed unused import

--- src/main/java/com/sylink/KodeKitten.java
+++ src/main/java/com/sylink/KodeKitten.java
@@ -17,7 +17,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
