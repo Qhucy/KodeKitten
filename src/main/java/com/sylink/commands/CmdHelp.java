6cf237c8c78d798ff60be1849f258cbaa09b3fc8
Qhucy <qhucy@protonmail.com>
2022-06-19 23:08:20 -0700

    Reverted back to normal functionality

--- src/main/java/com/sylink/commands/CmdHelp.java
+++ src/main/java/com/sylink/commands/CmdHelp.java
@@ -1,7 +1,6 @@
 package com.sylink.commands;
 
 import com.sylink.account.Account;
-import com.sylink.account.AccountManager;
 import lombok.NonNull;
 import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
@@ -41,40 +40,7 @@ public final class CmdHelp
     @Override
     public void onConsoleCommand(@NonNull final String label, @NonNull final String[] args)
     {
-        //System.out.println(helpMessage);
-
-        final Account account = AccountManager.getInstance().getAccount(259508942420443136L);
-
-        if (account == null)
-        {
-            System.out.println("null");
-            return;
-        }
-
-        if (args.length == 0)
-        {
-            System.out.println(account.getDiscordId() + " | " + account.getBalance() + " | " + account.getLastActivityTime());
-            return;
-        }
-
-        if (args[0].equalsIgnoreCase("1"))
-        {
-            account.addBalance(1.0);
-
-            System.out.println(account.getBalance());
-        }
-        else if (args[0].equalsIgnoreCase("2"))
-        {
-            AccountManager.getInstance().flushFromMemory(account, false);
-        }
-        else if (args[0].equalsIgnoreCase("3"))
-        {
-            System.out.println(AccountManager.getInstance().existsInMemory(account.getDiscordId()));
-        }
-        else if (args[0].equalsIgnoreCase("4"))
-        {
-            System.out.println(AccountManager.getInstance().existsInDatabase(account.getDiscordId()));
-        }
+        System.out.println(helpMessage);
     }
 
 }
