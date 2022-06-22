486c61b70486aeaacf0f09efb466e30556502705
Qhucy <qhucy@protonmail.com>
2022-06-21 23:14:24 -0700

    Unit tests for database console command

---
+++ src/test/java/com/sylink/commands/CmdDatabaseTest.java
@@ -0,0 +1,101 @@
+package com.sylink.commands;
+
+import com.sylink.account.Account;
+import com.sylink.account.AccountManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CmdDatabaseTest
+{
+
+    private final static String DATABASE_URL = "jdbc:sqlite:src/test/java/com/sylink/account/database_test.db";
+    private static AccountManager accountManager;
+
+    @BeforeAll
+    static void setUpAll()
+    {
+        accountManager = AccountManager.getInstance();
+        accountManager.openDatabaseConnection(DATABASE_URL);
+
+        Command.registerCommand(new CmdDatabase());
+    }
+
+    @Test
+    void createAccount()
+    {
+        Command.runCommands("db", new String[] {"create", "0"});
+
+        assertTrue(accountManager.existsInMemory(0L));
+        assertNotNull(accountManager.getAccount(0L));
+    }
+
+    @Test
+    void loadAccount()
+    {
+        assertTrue(accountManager.existsInDatabase(10L));
+
+        Command.runCommands("db", new String[] {"load", "10"});
+
+        assertTrue(accountManager.existsInMemory(10L));
+
+        Account account = accountManager.getAccount(10L);
+
+        assertNotNull(account);
+        assertEquals(10.0, account.getBalance());
+        assertTrue(account.hasPermission("admin"));
+        assertTrue(account.hasRole(10L));
+    }
+
+    @Test
+    void loadAccountWhenNotExist()
+    {
+        assertFalse(accountManager.existsInMemory(55L));
+        assertFalse(accountManager.existsInDatabase(55L));
+
+        Command.runCommands("db", new String[] {"load", "55"});
+
+        assertFalse(accountManager.existsInMemory(55L));
+    }
+
+    @Test
+    void saveAccount()
+    {
+        Account account = accountManager.getAccount(44L);
+
+        account.setBalance(5.0);
+
+        assertFalse(accountManager.existsInDatabase(44L));
+
+        Command.runCommands("db", new String[] {"save", "44"});
+
+        assertTrue(accountManager.existsInDatabase(44L));
+
+        accountManager.loadFromDatabase(44L);
+
+        assertTrue(accountManager.existsInMemory(44L));
+
+        account = accountManager.getAccount(44L);
+
+        assertNotNull(account);
+        assertEquals(5.0, account.getBalance());
+    }
+
+    @Test
+    void flushAccount()
+    {
+
+    }
+
+    @AfterAll
+    static void afterAll()
+    {
+        // Id 44 is used for testing and needs to be removed at the end.
+        accountManager.deleteFromDatabase(44L);
+
+        accountManager.closeDatabaseConnection();
+    }
+
+}
