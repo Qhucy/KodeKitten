e6c6880d41186a6c61d273cdab296d2b334033f4
Qhucy <qhucy@protonmail.com>
2022-06-28 11:34:51 -0700

    Added description comment

--- src/main/java/com/sylink/events/EventHandler.java
+++ src/main/java/com/sylink/events/EventHandler.java
@@ -3,7 +3,7 @@ package com.sylink.events;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 /**
- *
+ * Utility class that manages all triggered events in the program.
  */
 public final class EventHandler
         extends ListenerAdapter
