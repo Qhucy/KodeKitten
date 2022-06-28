eadaf23e4f1aa1fd0db6a0b21f36f8d0399e05d1
Qhucy <qhucy@protonmail.com>
2022-06-27 21:41:57 -0700

    Removed public fields and made class private

--- src/test/java/com/sylink/BotTest.java
+++ src/test/java/com/sylink/BotTest.java
@@ -9,15 +9,11 @@ import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BotTest
+class BotTest
 {
 
-    // The Discord Snowflake Id of the test bot.
-    public final static long TEST_BOT_ID = 890542435921440778L;
-    // The Discord Snowflake Id of the testing guild.
-    public final static long TEST_GUILD_ID = 989935294738485288L;
     // The test token.txt file used for unit testing.
-    public static final Path TOKEN_PATH = Paths.get("src/test/java/com/sylink/token_test.txt");
+    private static final Path TOKEN_PATH = Paths.get("src/test/java/com/sylink/token_test.txt");
     // The path to a blank text file used for unit testing.
     private static final Path BLANK_TOKEN_PATH = Paths.get("src/test/java/com/sylink/blank_token_test.txt");
     // An example invalid path used for unit testing.
