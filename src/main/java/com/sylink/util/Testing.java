99d96d70efdf2b3f13424002b1c4a20a32e2649b
Qhucy <qhucy@protonmail.com>
2022-06-29 09:07:43 -0700

    Removed redundant data

--- src/main/java/com/sylink/util/Testing.java
+++ src/main/java/com/sylink/util/Testing.java
@@ -2,28 +2,11 @@ package com.sylink.util;
 
 import com.sylink.Bot;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /**
  * Utility class used to aid in unit testing for the KodeKitten Testing Bot and Discord Guild.
  */
 public final class Testing
 {
-
-    /**
-     * =============
-     * | IMPORTANT |
-     * =============
-     * The path to the text file that contains the Discord Bot Token for the testing bot.
-     * This is used for unit testing and must be correct to run tests.
-     */
-    public static final Path TOKEN_PATH = Paths.get("../test_token.txt");
-    /**
-     * The path to the test snowflake config that contains the Discord Snowflake Ids
-     * for the unit test bot to use in the testing discord server.
-     */
-    public static final String SNOWFLAKE_PATH = "../test_snowflake.toml";
 
     // The Discord Snowflake Id of the test bot.
     public final static long BOT_ID = 890542435921440778L;
@@ -41,7 +24,7 @@ public final class Testing
      */
     public static Bot getBot()
     {
-        final String token = Bot.getTokenFromFile(TOKEN_PATH.toFile());
+        final String token = Bot.getTokenFromFile(Bot.TEST_TOKEN_PATH.toFile());
 
         return (token == null) ? null : new Bot(token);
     }
